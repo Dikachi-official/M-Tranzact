b9e7290afc12d0711b83b87d8fd34074d89eaccb
Dikachi-official <nwokontadikachi@gmail.com>
2024-01-11 03:33:18 +0100

    forgot password/ Email verification

--- userauth/admin.py
+++ userauth/admin.py
@@ -18,4 +18,5 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.register(User)
 admin.site.register(Support)
+admin.site.register(OtpToken)
 
--- userauth/urls.py
+++ userauth/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 app_name = "userauth"
@@ -20,6 +21,26 @@ urlpatterns = [
 
     #CHANGE PASSWORD
     path('changepassword/', views.change_password, name='change-password'),
+
+
+    # PASSWORD RESET URLS
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='authentication/password_reset_form.html', 
+        email_template_name='authentication/password_reset_email.html',
+        success_url=reverse_lazy('userauth:password_reset_done')), 
+        name='password_reset'),
+        
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html', ), name='password_reset_done'),
+
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='authentication/password_reset_confirm.html', 
+        success_url=reverse_lazy('userauth:password_reset_complete', )), 
+        name='password_reset_confirm'),
+
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html', ), name='password_reset_complete'),
+
+
+
 
 
 ]
